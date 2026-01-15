e94fedff79116cfee3bf1ca5ddd748053140f429
CheneyDev <cheneydev@gmail.com>
2026-01-15 18:03:43 +0800

    Initial Commit

---
+++ TongJuanAlter/TongJuanAlterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TongJuanAlterApp.swift
+//  TongJuanAlter
+//
+//  Created by Cheney on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct TongJuanAlterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
